5233413dcf8b43c6d6a5c7d1a904d10f1bcd9dd7
Xingxin HE <xingxin.he@connect.ust.hk>
2025-11-26 15:39:09 +0800

    format once

--- util/check_pcd.py
+++ util/check_pcd.py
@@ -3,9 +3,9 @@ import numpy as np
 import os
 
 # point = np.loadtxt('./example_image/pcd_scene_020.txt')
-point = np.loadtxt('./example_image/pcd_crop_020.txt')
+point = np.loadtxt("./example_image/pcd_crop_020.txt")
 pcd = o3d.geometry.PointCloud()
-point = point[:,:3]
+point = point[:, :3]
 pcd.points = o3d.utility.Vector3dVector(point)
 # o3d.visualization.draw_geometries([pcd])
 
@@ -13,11 +13,12 @@ vis = o3d.visualization.Visualizer()
 vis.create_window()
 vis.add_geometry(pcd)
 
+
 def rotate_view(vis):
     ctr = vis.get_view_control()
     ctr.rotate(10.0, 0.0)
     return False
 
+
 while True:
-    o3d.visualization.draw_geometries_with_animation_callback([pcd],
-                                                            rotate_view)
+    o3d.visualization.draw_geometries_with_animation_callback([pcd], rotate_view)
--- util/convert_obj_convexhull.py
+++ util/convert_obj_convexhull.py
@@ -8,8 +8,8 @@ OUTPUT_DATA_FILENAME = "IPARing_collision.obj"
 LOG_FILENAME = "IPARing_convert.log"
 
 p.connect(p.DIRECT)
-name_in = os.path.join(DATA_FOLDER,INPUT_DATA_FILENAME)
-name_out = os.path.join(DATA_FOLDER,OUTPUT_DATA_FILENAME)
-name_log = os.path.join(DATA_FOLDER,LOG_FILENAME)
+name_in = os.path.join(DATA_FOLDER, INPUT_DATA_FILENAME)
+name_out = os.path.join(DATA_FOLDER, OUTPUT_DATA_FILENAME)
+name_log = os.path.join(DATA_FOLDER, LOG_FILENAME)
 
-p.vhacd(name_in, name_out, name_log, alpha=0.04,resolution=50000 )
+p.vhacd(name_in, name_out, name_log, alpha=0.04, resolution=50000)
